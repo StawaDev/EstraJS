d703acebe6d5e099f943ac88867bd71197df6007
unknown <damodaraswarupa@gmail.com>
2022-01-22 13:33:48 +0800

    Added More Functions

--- index.js
+++ index.js
@@ -1,88 +1,171 @@
-import { BASE_URL , get_api, produce } from './base.js';
+import { BASE_URL, get_api, produce } from './base.js';
 
 export class EstraClient {
     constructor() {
         this.sfw = {
-            async run(generate=Number) {
-                var Url = `${BASE_URL}/sfw/run`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
+                async run(generate = Number) {
+                    var Url = 'sfw/run'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
 
-                return get_api(Url, "link");
+                    return get_api(Url, false, 'link');
+                },
+
+                async hug(generate = Number) {
+                    var Url = 'sfw/hug'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async smile(generate = Number) {
+                    var Url = 'sfw/smile'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async headpat(generate = Number) {
+                    var Url = 'sfw/headpat'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async poke(generate = Number) {
+                    var Url = 'sfw/poke'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async bite(generate = Number) {
+                    var Url = 'sfw/bite'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async neko(generate = Number) {
+                    var Url = 'sfw/neko'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async highfive(generate = Number) {
+                    var Url = 'sfw/headpat'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async slap(generate = Number) {
+                    var Url = 'sfw/slap'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                }
             },
 
-            async hug(generate=Number) {
-                var Url = `${BASE_URL}/sfw/hug`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
+            this.nsfw = {
+                async kill(generate = Number) {
+                    var Url = 'nsfw/kill'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
 
-                return get_api(Url, "link");
+                    return get_api(Url, false, 'link');
+                },
+
+                async yuri(generate = Number) {
+                    var Url = 'nsfw/yuri'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                },
+
+                async yaoi(generate = Number) {
+                    var Url = 'nsfw/yaoi'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'link');
+                    }
+
+                    return get_api(Url, false, 'link');
+                }
             },
 
-            async smile(generate=Number) {
-                var Url = `${BASE_URL}/sfw/smile`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
+            this.games = {
+                async truth(generate = Number) {
+                    var Url = 'games/truth'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'text');
+                    }
 
-                return get_api(Url, "link");
+                    return get_api(Url, false, 'text');
+                },
+
+                async dare(generate = Number) {
+                    var Url = 'games/dare'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'text');
+                    }
+
+                    return get_api(Url, false, 'text');
+                }
             },
 
-            async headpat(generate=Number) {
-                var Url = `${BASE_URL}/sfw/headpat`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
+            this.anigames = {
+                async truth(generate = Number) {
+                    var Url = 'anigames/truth'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'text');
+                    }
+                    return get_api(Url, false, 'text');
+                },
+
+                async dare(generate = Number) {
+                    var Url = 'anigames/dare'
+                    if (typeof generate === 'number') {
+                        return produce(generate, Url, 'text');
+                    }
+                    return get_api(Url, false, 'text');
+                },
+
+                async waifu() {
+                    return get_api('anigames/waifu', true);
+                },
+
+                async husbando() {
+                    return get_api('anigames/husbando', true);
+                },
+
+                async shipper_waifu(player = String) {
+                    return get_api(`anigames/shipper/waifu/?player=${player}`, true);
+                },
+
+                async shipper_husbando(player) {
+                    return get_api(`anigames/shipper/husbando/?player=${player}`, true);
                 }
-
-                return get_api(Url, "link");
-            },
-
-            async poke(generate=Number) {
-                var Url = `${BASE_URL}/sfw/poke`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
-
-                return get_api(Url, "link");
-            },
-
-            async bite(generate=Number) {
-                var Url = `${BASE_URL}/sfw/bite`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
-
-                return get_api(Url, "link");
-            },
-
-            async neko(generate=Number) {
-                var Url = `${BASE_URL}/sfw/neko`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
-
-                return get_api(Url, "link");
-            },
-
-            async highfive(generate=Number) {
-                var Url = `${BASE_URL}/sfw/headpat`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
-
-                return get_api(Url, "link");
-            },
-
-            async slap(generate=Number) {
-                var Url = `${BASE_URL}/sfw/slap`
-                if (typeof generate === "number") {
-                    return produce(generate, Url, "link");
-                }
-
-                return get_api(Url, "link");
-            }
-        };
+            };
     };
 };
