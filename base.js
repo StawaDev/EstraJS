e6a9780a51631b871593f8a3a3740f11e66c3cbc
unknown <damodaraswarupa@gmail.com>
2022-01-22 13:33:34 +0800

    Updated Http Function

--- base.js
+++ base.js
@@ -1,17 +1,18 @@
 import fetch from "node-fetch";
 
-export var BASE_URL = "https://estra-api.herokuapp.com/api";
+export var BASE_URL = "https://estra-api.herokuapp.com/api/";
 
-export async function get_api(Url=String, Type=String) {
-    const response = await fetch(Url);
+export async function get_api(Url=String, JsonType=Boolean, Type=String) {
+    const response = await fetch(BASE_URL + Url);
     if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`);
+        throw new Error(`Http Error, ${response.status}`);
     };
     const json = await response.text();
     const obj = JSON.parse(json);
-    if (Type === "link") {
-        return obj.link;
+    if (JsonType === false) {
+        return obj[Type];
     }
+    return obj;
 };
 
 export async function produce(Total=Number, Url=String, Type=String) {
@@ -20,9 +21,13 @@ export async function produce(Total=Number, Url=String, Type=String) {
     if (Total > 10) {
         throw new Error('Total must be lower than 10')
     }
+    if (Total === 0 || (Total === 1)) {
+        throw new Error('Total must be greater than 1')
+    }
+
     while (true) {
         i++;
-        const API = await get_api(Url, Type);
+        const API = await get_api(Url, false, Type);
         LIST.push(API)
         if (i === Total) break;
     }
