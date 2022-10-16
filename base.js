9aeca2612a4ca806b0721247acdb24b22b1d8040
Stawa <77476824+StawaDev@users.noreply.github.com>
2022-10-16 11:16:13 +0800

    Update & Delete

--- base.js
+++ base.js
@@ -1,20 +1,9 @@
-import { fileURLToPath } from 'url';
 import { get_api } from "./http.js";
 import { InvalidNumber } from "./errors.js";
-import fetch from "node-fetch";
-import fs from "fs";
-import path from 'path';
-import open from 'open';
 
 export var base_url = "https://estra-api.vercel.app/api/v1/";
 
 export class Base {
-    constructor () {
-        this._filename = fileURLToPath(import.meta.url);
-        this._dirname = path.dirname(this._filename);
-        this.PathImage = this._dirname + '\\shipper.jpg'
-    }
-
     async produce(total = Number, route = String, type = String) {
         const generated_urls = []
         var i = 0;
@@ -33,24 +22,5 @@ export class Base {
         }
         return generated_urls
     };
-
-    async download(url = String) {
-        const response = await fetch(url);
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-        fs.writeFileSync(this.PathImage, buffer);
-    }
-
-    async get_image(url = String, save = Boolean, open_file = Boolean) {
-        if (save === true && open_file === false) {
-            return await download(url);
-        }
-        if (save === false || open_file === false) {
-            return;
-        }
-        if (save === true || open_file === true) {
-            return await download(url), open(this.PathImage);
-        }
-    }
 
 }
--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 import { AniGames } from './category/anigames.js';
 import { Games } from './category/games.js';
-import { OsuClient } from './category/osu.js';
 import { Nsfw } from './category/nsfw.js';
 import { Sfw } from './category/sfw.js';
 import { Help } from './help.js';
@@ -11,7 +10,6 @@ export class EstraClient {
         this.Nsfw = new Nsfw();
         this.Games = new Games();
         this.AniGames = new AniGames();
-        this.OsuClient = new OsuClient();
         this.Help = new Help();
     };
 };
